a3364aae520d1ff7f353a403f96caae9d128fed6
icebear2n2 <icebear2n2@gmail.com>
2023-08-21 18:31:50 +0900

    fix: 맛집상세 정보 컴포넌트 코드 정리

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Map from "./Map";
 import PlaceMap from "./Map";
 
 const Information = () => {
@@ -45,18 +44,6 @@ const Information = () => {
           <li>가게 정보: </li>
           <li>추천 아이디 : </li>
         </ul>
-
-        {/*<img*/}
-        {/*  style={{*/}
-        {/*    marginTop: "52px",*/}
-        {/*    width: "100%",*/}
-        {/*    height: "auto",*/}
-        {/*    textAlign: "center",*/}
-        {/*    marginBottom: "42px",*/}
-        {/*  }}*/}
-        {/*  src="https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f208d39e-c165-4dcd-b5d3-f36743e0a359"*/}
-        {/*  alt="movie"*/}
-        {/*/>*/}
         <PlaceMap />
         <hr />
       </div>

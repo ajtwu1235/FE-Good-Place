f96bc0b202be9adfa34cf2edb40cc68f27c75043
icebear2n2 <icebear2n2@gmail.com>
2023-08-21 18:30:57 +0900

    fix: 맛집 추가 페이지 코드 정리

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,3 +1,48 @@
+const RotatedDiv = () => {
+  return (
+    <div
+      style={{
+        width: "285px",
+        height: "380px",
+        transform: "rotate(90deg)",
+        borderRadius: "20px",
+        background: "#D9D9D9",
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "0 60px",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="78"
+        height="78"
+        viewBox="0 0 78 78"
+        fill="none"
+      >
+        <rect
+          x="78"
+          y="33.15"
+          width="11.7"
+          height="78"
+          rx="5.85"
+          transform="rotate(90 78 33.15)"
+          fill="#A8A8A8"
+        />
+        <rect
+          x="44.8496"
+          y="78"
+          width="11.7"
+          height="78"
+          rx="5.85"
+          transform="rotate(-180 44.8496 78)"
+          fill="#A8A8A8"
+        />
+      </svg>
+    </div>
+  );
+};
+
 const Recommend = () => {
   return (
     <>
@@ -42,255 +87,10 @@ const Recommend = () => {
           </li>
         </ul>
       </div>
-      <div style={{ textAlign: "center" }}>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "65px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "92px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "92px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "65px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "92px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
-        <div
-          style={{
-            width: "285px",
-            height: "380px",
-            transform: "rotate(90deg)",
-            borderRadius: "20px",
-            background: "#D9D9D9",
-            marginLeft: "92px", // 변경된 부분
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            marginRight: "48px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="78"
-            height="78"
-            viewBox="0 0 78 78"
-            fill="none"
-          >
-            <rect
-              x="78"
-              y="33.15"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(90 78 33.15)"
-              fill="#A8A8A8"
-            />
-            <rect
-              x="44.8496"
-              y="78"
-              width="11.7"
-              height="78"
-              rx="5.85"
-              transform="rotate(-180 44.8496 78)"
-              fill="#A8A8A8"
-            />
-          </svg>
-        </div>
+      <div style={{ textAlign: "center" }} className="place insert">
+        {[1, 2, 3].map((item) => (
+          <RotatedDiv key={item} />
+        ))}
       </div>
     </>
   );

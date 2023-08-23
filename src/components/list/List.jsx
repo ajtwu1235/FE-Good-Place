bb66627124254b721506257b6d0be94023ec87ad
Amy <wlgpek90@naver.com>
2023-08-23 15:05:56 +0900

    list center

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -11,28 +11,31 @@ const ListPage = () => {
       <div></div>
 
       <div className="StoryListBox">
-        <div className="topTitleStyle">
-          <p>믿고보는 맛집 리스트</p>
-        </div>
-
-        <div className="buttonList">
-          <button className="buttonStyle"># 제주</button>
-          <button className="buttonStyle"># 분식</button>
-          <button className="buttonStyle"># 집밥</button>
-        </div>
-
-        {/* 여기서부터 한줄 시작 */}
-        <div className="flexRow">
-          <div>
-            <p className="titleStyle">제주도 제주시</p>
-            <p className="subTitleStyle">신해바라기 분식</p>
-            <img src="images/sunflower.png"></img>
+        <div className="story_container">
+          <div className="story_wrap">
+            <div className="topTitleStyle">
+              <p>믿고보는 맛집 리스트</p>
+            </div>
+            <div className="buttonList">
+              <button className="buttonStyle"># 제주</button>
+              <button className="buttonStyle"># 분식</button>
+              <button className="buttonStyle"># 집밥</button>
+            </div>
           </div>
 
-          <div>
-            <p className="titleStyle">제주도 제주시</p>
-            <p className="subTitleStyle">신해바라기 분식</p>
-            <img src="images/sunflower.png"></img>
+          {/* 여기서부터 한줄 시작 */}
+          <div className="flexRow">
+            <div>
+              <p className="titleStyle">제주도 제주시</p>
+              <p className="subTitleStyle">신해바라기 분식</p>
+              <img src="images/sunflower.png"></img>
+            </div>
+
+            <div>
+              <p className="titleStyle">제주도 제주시</p>
+              <p className="subTitleStyle">신해바라기 분식</p>
+              <img src="images/sunflower.png"></img>
+            </div>
           </div>
         </div>
 

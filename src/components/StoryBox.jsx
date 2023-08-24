e22dfe1145bd1db5043ac5adda51cc5049f6335b
ajtwu1235 <ajtwu1235@gmail.com>
2023-08-24 17:48:24 +0900

    adfad

--- src/components/StoryBox.jsx
+++ src/components/StoryBox.jsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from "react";
 import "./story.css";
+import axios from "axios";
 
 const StoryBox = () => {
+
+  const [data,setData]= useState([])
+
+  const getData = () =>
+    axios.get("http://localhost:8080/favorite").then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    });
+
+    useEffect(() => {
+      getData();
+    }, []);
+  
+
+
   return (
     <>
-      <div className="storyBox">
-        <div className="storybox_wrap">
-          <img
-            src="/images/friedpork.png"
-            alt="pork"
-            className="story_food_img"
-          />
-          <div className="storybox_text">
+    
+       
+          {data.map(el=>(
+              <div className="storyBox">
+             <div className="storybox_wrap">
+             <img
+               src="/images/friedpork.png"
+               alt="pork"
+               className="story_food_img"
+             />
+            
+            <div className="storybox_text">
             <div className="storybox_wrap1">
               <p className="story_text story_text1">Editer.</p>
-              <p className="story_text story_text2">현수정</p>
+              <p className="story_text story_text2">{el.username}</p>
               <img
                 src="/images/heart-1.svg"
                 alt="heart"
@@ -21,7 +42,7 @@ const StoryBox = () => {
               />
             </div>
             <p className="story_text story_title">
-              수정이가 선택한 제주맛집 10곳
+            {el.username}이가 선택한 제주맛집 {el.count}곳
             </p>
             <p className="story_text story_title2">제주 인기 터짐!</p>
             <div className="storybox_wrap2">
@@ -35,8 +56,11 @@ const StoryBox = () => {
               <span className="story_text story_like_count">123</span>
             </div>
           </div>
-        </div>
-      </div>
+           </div>
+           </div>
+          ))}
+       
+    
     </>
   );
 };

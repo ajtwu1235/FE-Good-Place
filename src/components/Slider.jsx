19f5cdc32031164f480c83ebbc8b06a06bc0a80c
icebear2n2 <icebear2n2@gmail.com>
2023-08-24 17:30:32 +0900

    feat: 파이어베이스 사진 목록 가져오기 구현 완료

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,22 @@
-import "./detail.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import "./styles.css";
 import Information from "./Information";
 import DetailReview from "./DetailReview";
 import DetailComment from "./DetailComment";
 import DetailCommentInput from "./DetailCommentInput";
 import { useParams } from "react-router";
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+
+import "./detail.css";
+import firebaseConfig from "./FirebaseConfig"; // Import the FirebaseConfig component
+import { initializeApp } from "firebase/app"; // Import initializeApp directly from "firebase/app"
+
+// Initialize Firebase using the configuration from FirebaseConfig
+initializeApp(firebaseConfig);
+// Initialize Firebase
+// Initialize Firebase
+
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
@@ -25,31 +34,55 @@ const responsive = {
     slidesToSlide: 1, // optional, default to 1.
   },
 };
-const sliderImageUrl = [
-  //First image url
-  {
-    url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
-  },
-  {
-    url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
-  },
-  //Second image url
-  {
-    url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
-  },
-  //Third image url
-  {
-    url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
-  },
+// const sliderImageUrl = [
+//   //First image url
+//   {
+//     url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
+//   },
+//   {
+//     url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
+//   },
+//   //Second image url
+//   {
+//     url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
+//   },
+//   //Third image url
+//   {
+//     url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
+//   },
+//
+//   //Fourth image url
+//
+//   {
+//     url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
+//   },
+// ];
 
-  //Fourth image url
-
-  {
-    url: "https://github.com/PLAYDATA-GOOD-PLACE/FE-Good-Place/assets/87232411/f029942a-27a4-4121-a0bc-4cc3320ad1bb",
-  },
-];
 const Slider = () => {
   const { placeId } = useParams();
+  const [sliderImageUrl, setSliderImageUrl] = useState([]);
+  useEffect(() => {
+    // Reference to the Firebase storage bucket
+    const storage = getStorage();
+    const storageRef = ref(storage, "/" + placeId);
+
+    const fetchImageUrls = async () => {
+      try {
+        const imageRefs = await listAll(storageRef);
+        const urls = await Promise.all(
+          imageRefs.items.map(async (item) => {
+            const url = await getDownloadURL(item);
+            return { url };
+          }),
+        );
+        setSliderImageUrl(urls);
+      } catch (error) {
+        console.error("Error fetching image URLs from Firebase:", error);
+      }
+    };
+
+    fetchImageUrls();
+  }, [placeId]);
   return (
     <>
       <div

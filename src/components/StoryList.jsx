fc5470b5ded4ec12763e827695c16050b3b626c3
icebear2n2 <icebear2n2@gmail.com>
2023-08-24 11:26:56 +0900

    feat: mainStory에 리뷰 연결 완료

--- src/components/StoryList.jsx
+++ src/components/StoryList.jsx
@@ -1,4 +1,6 @@
 import "./story.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const StoryList = () => {
   const StoryListBox = {
     width: "442px",
@@ -19,76 +21,31 @@ const StoryList = () => {
     color: "#1E1E1E",
   };
 
+  const [data, setData] = useState([]);
+
+  const getData = () =>
+    axios.get("http://localhost:8080/api/v1/client?size=8").then((response) => {
+      console.log(response.data);
+      setData(response.data.content);
+    });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
   return (
     <>
       <div style={StoryListBox}>
         <div style={StoryListBox}>
-          <p style={titleStyle}>제주도 제주시</p>
-          <p style={subTitleStyle}>신해바라기 분식</p>
-          <img src="images/sunflower.png"></img>
-          <p>
-            풍자 또간집으로 유명한 신해바라기 분식. 일요일 저녁 5시 반쯤
-            방문하니 만석이고 앞에 웨이팅 한 팀 있어서 10여분 정도 기다리다가
-            들어갔어요. 오징어 젓갈이 진짜 맛있어서 그것만으로 밥 반공기는
-            먹은거 같아요. 순두부찌개는 순두부가 초당순두부처럼 몽글몽글한
-            타입인데 진짜 고소해요. 자극적이라 넘 맛있게 먹었고 맵찔이라
-            기본맛으로 먹어도 충분히 매웠어요. 근데 맛있게 매웠습니다. 냄비우동
-            자체는 크게 특별한 점은 없는데 이게 말이 우동이지 실제로는 우동면이
-            아니어서 요 냄비우동의 면을 순두부에 넣어서 먹으면 그게 또
-            별미에요~! 둘이서 순두부 2개 기본맛에 냄비우동 하나 시켜서 면 나눠서
-            먹었어요ㅎㅎ 진짜 맛있어요
-          </p>
-        </div>
-        <div style={StoryListBox}>
-          <p style={titleStyle}>제주도 제주시</p>
-          <p style={subTitleStyle}>신해바라기 분식</p>
-          <img src="images/sunflower.png"></img>
-          <p>
-            풍자 또간집으로 유명한 신해바라기 분식. 일요일 저녁 5시 반쯤
-            방문하니 만석이고 앞에 웨이팅 한 팀 있어서 10여분 정도 기다리다가
-            들어갔어요. 오징어 젓갈이 진짜 맛있어서 그것만으로 밥 반공기는
-            먹은거 같아요. 순두부찌개는 순두부가 초당순두부처럼 몽글몽글한
-            타입인데 진짜 고소해요. 자극적이라 넘 맛있게 먹었고 맵찔이라
-            기본맛으로 먹어도 충분히 매웠어요. 근데 맛있게 매웠습니다. 냄비우동
-            자체는 크게 특별한 점은 없는데 이게 말이 우동이지 실제로는 우동면이
-            아니어서 요 냄비우동의 면을 순두부에 넣어서 먹으면 그게 또
-            별미에요~! 둘이서 순두부 2개 기본맛에 냄비우동 하나 시켜서 면 나눠서
-            먹었어요ㅎㅎ 진짜 맛있어요
-          </p>
-        </div>
-        <div style={StoryListBox}>
-          <p style={titleStyle}>제주도 제주시</p>
-          <p style={subTitleStyle}>신해바라기 분식</p>
-          <img src="images/sunflower.png"></img>
-          <p>
-            풍자 또간집으로 유명한 신해바라기 분식. 일요일 저녁 5시 반쯤
-            방문하니 만석이고 앞에 웨이팅 한 팀 있어서 10여분 정도 기다리다가
-            들어갔어요. 오징어 젓갈이 진짜 맛있어서 그것만으로 밥 반공기는
-            먹은거 같아요. 순두부찌개는 순두부가 초당순두부처럼 몽글몽글한
-            타입인데 진짜 고소해요. 자극적이라 넘 맛있게 먹었고 맵찔이라
-            기본맛으로 먹어도 충분히 매웠어요. 근데 맛있게 매웠습니다. 냄비우동
-            자체는 크게 특별한 점은 없는데 이게 말이 우동이지 실제로는 우동면이
-            아니어서 요 냄비우동의 면을 순두부에 넣어서 먹으면 그게 또
-            별미에요~! 둘이서 순두부 2개 기본맛에 냄비우동 하나 시켜서 면 나눠서
-            먹었어요ㅎㅎ 진짜 맛있어요
-          </p>
-        </div>
-        <div style={StoryListBox}>
-          <p style={titleStyle}>제주도 제주시</p>
-          <p style={subTitleStyle}>신해바라기 분식</p>
-          <img src="images/sunflower.png"></img>
-          <p>
-            풍자 또간집으로 유명한 신해바라기 분식. 일요일 저녁 5시 반쯤
-            방문하니 만석이고 앞에 웨이팅 한 팀 있어서 10여분 정도 기다리다가
-            들어갔어요. 오징어 젓갈이 진짜 맛있어서 그것만으로 밥 반공기는
-            먹은거 같아요. 순두부찌개는 순두부가 초당순두부처럼 몽글몽글한
-            타입인데 진짜 고소해요. 자극적이라 넘 맛있게 먹었고 맵찔이라
-            기본맛으로 먹어도 충분히 매웠어요. 근데 맛있게 매웠습니다. 냄비우동
-            자체는 크게 특별한 점은 없는데 이게 말이 우동이지 실제로는 우동면이
-            아니어서 요 냄비우동의 면을 순두부에 넣어서 먹으면 그게 또
-            별미에요~! 둘이서 순두부 2개 기본맛에 냄비우동 하나 시켜서 면 나눠서
-            먹었어요ㅎㅎ 진짜 맛있어요
-          </p>
+          {data.map((el) => (
+            <div key={el.id}>
+              <p style={titleStyle}>{el.address}</p>
+              <p style={subTitleStyle}>{el.name}</p>
+              <img src="images/sunflower.png"></img>
+              <p>{el.myReview}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
